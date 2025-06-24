7416314d57312e4dbafa3f9b685b3815277fd4d8
pallavimohite <157053995+pallavimohite@users.noreply.github.com>
2025-06-24 09:59:51 +0530

    Add files via upload

---
+++ Basic_python/ope.py
@@ -0,0 +1,91 @@
+#1.Integer,float,tuple,bool,dict,set and String cant add in list 
+#2.only add list in list(add one list in front of another list ) also tuple add in another tuple also
+#3.We dont add two set by using + also dict
+#4.we only bool with int,float not string ,dict,list,tuple,set
+a=20
+b=30
+c=2.0
+d="Pallavi"
+d1="Mohite"
+e=["CPU","Mouse","Printer"]
+f=["IT","Mechanical","ETC"]
+t=("Teacher","Student","school")
+t1=(1,2,34)
+B=True
+F=False
+s={1,2,3,4,5,7,8,9,10}
+s1={6,7,8,9}
+D={"Name":"Pallvi","Age":43}
+D1={"Name":"Rutija","Roll":36}
+
+#Addition operator
+# print(a+b)
+# print(b+c)
+# print(e+f)
+# print(t+t1)
+#print(B+a)
+#print(B+d)
+#print(B+e)
+#print(B+s)
+
+#Multiplication operator
+#String,list,tuple,dict,set not multiply with itself
+#integer multiply with float,list,string,tuple and itself
+#Integer not multiply with swt and dict
+#set,dic,list not multiply with each other
+#multiply bool with int,float,list,tuple,string. but not with dict,set
+# print(a*b)
+# print(b*c)
+# print(a*d)
+# print(a*e)
+#print(a*t)
+# print(a*B)
+# print(B*e)
+# print(f*e)
+# print(B*t)
+#print(B*s)
+#print(B*D)
+#print(B*d)
+
+#we substract int,set,float with itself
+# print(s-s1)
+# print(a-b)
+
+#Division operator use in int,float,bool with itself
+# print(a/b)
+# print(a/c)
+#print(a/d)
+#print(a/f)
+#print(a/t)
+#print(a/s)
+#print(a/D)
+#print(d/d1)
+#print(d/e)
+#print(e/f)
+#print(d/t)
+#print(a/B)
+
+
+# print(a//b)
+# print(a//c)
+# print(a//d)
+#print(a//f)
+print(a//t)
+print(a//s)
+print(a//D)
+print(d//d1)
+print(d//e)
+print(e//f)
+print(d//t)
+print(a//B)
+
+
+
+
+
+
+
+
+
+
+
